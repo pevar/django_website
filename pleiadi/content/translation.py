6efc1abb5eb1cdba4431451418fac54c49cefd20
claudio <ilnano>
2014-03-10 15:36:46 +0100

    pleiadi app moved in libs

--- pleiadi/content/translation.py
+++
@@ -1,6 +0,0 @@
-from pleiadi.seo.translation import SeoTranslationOptions
-
-
-class BaseTranslationOptions(SeoTranslationOptions):
-    fields = ('title', 'slug', 'description', 'abstract', 'visible')
-    empty_values = {'slug': ''}
--- pleiadi/seo/translation.py
+++
@@ -1,5 +0,0 @@
-from modeltranslation.translator import TranslationOptions
-
-
-class SeoTranslationOptions(TranslationOptions):
-    fields = ('seo_title', 'seo_description', 'seo_keywords',)
